1bde0a035dd5ed0012a27697277cc17027d79667
Patrick Miroglio <34417947+P-mir@users.noreply.github.com>
2025-09-04 10:18:00 +0200

    Initialize project structure

---
+++ src/utils/__init__.py
@@ -0,0 +1,17 @@
+from .helpers import load_json, save_json, update_metrics
+from .logging_config import setup_logger
+from .models import LegalArticle, QueryResponse, RetrievalResult, TextChunk
+from .types import EmbeddingMatrix, EmbeddingVector
+
+__all__ = [
+    'EmbeddingVector',
+    'EmbeddingMatrix',
+    'LegalArticle',
+    'TextChunk',
+    'RetrievalResult',
+    'QueryResponse',
+    'setup_logger',
+    'save_json',
+    'load_json',
+    'update_metrics',
+]
---
+++ src/utils/types.py
@@ -0,0 +1,9 @@
+
+from typing import TypeAlias
+
+import numpy as np
+import numpy.typing as npt
+
+# Type aliases for embeddings
+EmbeddingVector: TypeAlias = npt.NDArray[np.float32]
+EmbeddingMatrix: TypeAlias = npt.NDArray[np.float32]
