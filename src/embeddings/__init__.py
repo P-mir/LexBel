835abbabc9dd30b9bae255009ea6d9d760193800
Patrick Miroglio <34417947+P-mir@users.noreply.github.com>
2025-09-07 15:47:00 +0200

    Create embedding abstractions

---
+++ src/embeddings/__init__.py
@@ -0,0 +1,17 @@
+from embeddings.base import (
+    BaseEmbedder,
+    EmbeddingError,
+    EmbeddingGenerationError,
+    ModelLoadError,
+)
+from embeddings.cloud_embedder import CloudEmbedder
+from embeddings.local_embedder import LocalEmbedder
+
+__all__ = [
+    'BaseEmbedder',
+    'EmbeddingError',
+    'ModelLoadError',
+    'EmbeddingGenerationError',
+    'LocalEmbedder',
+    'CloudEmbedder',
+]
