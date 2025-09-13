a0a91acdb324da41ef9ff7d64bf0d849b1f90063
Patrick Miroglio <34417947+P-mir@users.noreply.github.com>
2025-09-13 15:42:00 +0200

    Start retriever module with MMR

---
+++ src/retrievers/__init__.py
@@ -0,0 +1,10 @@
+"""Retrievers package initialization."""
+
+from retrievers.hybrid import HybridRetriever
+from retrievers.mmr import MMRRetriever, mmr_select
+
+__all__ = [
+    'mmr_select',
+    'MMRRetriever',
+    'HybridRetriever',
+]
