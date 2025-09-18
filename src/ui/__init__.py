c96b6486f01de8b5de97c28723ca4421b64a7ce3
Patrick Miroglio <34417947+P-mir@users.noreply.github.com>
2025-09-18 09:24:00 +0200

    Start UI module with styling

---
+++ src/ui/__init__.py
@@ -0,0 +1,6 @@
+"""UI components package for LexBel."""
+
+from ui.dashboard import render_dashboard
+from ui.styling import get_custom_css, get_lexbel_logo_html
+
+__all__ = ['get_custom_css', 'get_lexbel_logo_html', 'render_dashboard']
